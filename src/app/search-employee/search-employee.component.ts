f716f3638d972aa5d421df18449f8698eafce4a3
akhil <akhil.paul@ymail.com>
2022-12-07 15:48:15 +0530

    Adds search feature

--- src/app/search-employee/search-employee.component.ts
+++ src/app/search-employee/search-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-search-employee',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-employee.component.css']
 })
 export class SearchEmployeeComponent {
-
+  constructor(private api:ApiService){}
+  empCode=""
+  searchData:any=[]
+  readValue=()=>{
+    let data = {"empCode":this.empCode}
+    console.log(data)
+    this.api.searchEmployee(data).subscribe(
+      (response:any)=>{
+        console.log(response)
+        if(response.length==0){
+          alert("No result")
+        }
+        else{
+          this.searchData=response
+        }
+      }
+    )
+  }
+  
 }
